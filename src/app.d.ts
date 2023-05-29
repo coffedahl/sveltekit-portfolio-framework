68148cb9e085546fbbc0d10c858bd37492e5e363
Christoffer Dahl <coffedahl@hotmail.com>
2023-05-30 00:00:19 +0200

    Updated locals type

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { Database } from '$lib/db';
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			db: Database;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
