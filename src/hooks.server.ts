7237d0d91ce72a21606d94fc2b40b3e63edadae9
Christoffer Dahl <coffedahl@hotmail.com>
2023-05-30 00:01:25 +0200

    Updated to use the new session class

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,30 +1,32 @@
 // Imports
 import { redirect, type Handle } from '@sveltejs/kit';
-import { Database } from './lib/db'
-import { validateSession, parseSession } from '$lib/session';
+import { Database } from './lib/db';
+import { Session } from '$lib/session';
 // Create mock databse
-const db = new Database()
+const db = new Database();
 
 // Code running every server request
 export const handle = (async ({ event, resolve }) => {
 	// Link mock db
-	event.locals.db = db
+	event.locals.db = db;
 	// If user tries to acces admin path
 	if (event.url.pathname.startsWith('/admin')) {
 		// Get session
-		const session = event.cookies.get('session')
+		const session = event.cookies.get('session');
 		// Check if session is not there
 		if (!session) {
 			// Redirect to login
-			throw redirect(303, '/login')
-
-		} else
-			// If not validated session
-			if (!validateSession(parseSession(JSON.parse(String(session))))) {
+			throw redirect(303, '/login');
+		} else {
+			// Create new session object
+			const sess = Session.createFromObject(JSON.parse(String(session)));
+			// Check if session is validated
+			if (!sess.validateSession()) {
 				// Delete cookie and redirect
-				event.cookies.delete('session')
-				throw redirect(303, '/login')
+				event.cookies.delete('session');
+				throw redirect(303, '/login');
 			}
+		}
 	}
 	// Resolve event
 	const response = await resolve(event);
