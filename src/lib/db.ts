5ba64955b983e8ca7028d35d33e985f8db7caad6
Christoffer Dahl <coffedahl@hotmail.com>
2023-06-02 13:19:22 +0200

    Added multiple functions for inteaction with the database

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,6 @@
 import { Surreal } from 'surrealdb.js';
+import { Session } from './classes/session';
+import { User } from './classes/user';
 
 /**
  * Database handler
@@ -17,6 +19,54 @@ export class Database {
 		this._db.use(ns, db);
 	}
 
+	/**
+	 * USERS
+	 */
+	async getUserByUsername(username: string): Promise<User> {
+		const response = await this._db.query('SELECT * FROM user WHERE username=$username;', {
+			username
+		});
+		if (response[0].result[0]) {
+			return User.createFromObject(response[0].result[0]);
+		} else {
+			throw new Error('No user with username: ' + username + ' was found.');
+		}
+	}
+
+	/**
+	 * SESSIONS
+	 */
+	async getSessionBySessionId(sessionId: string): Promise<Session> {
+		const response = this._db.query('SELECT * FROM $sessionId;', {
+			sessionId: 'session:' + sessionId
+		});
+		if (response[0].result[0]) {
+			return Session.createFromObject(response[0].result[0]);
+		} else {
+			throw new Error('Session with id: ' + sessionId + ' was not found');
+		}
+	}
+	async createSession(username: string): Promise<Session> {
+		const session = Session.createSession(username);
+		const response = this._db.query('CREATE $sessionId SET username=$username, expires=$expires;', {
+			sessionId: session.sessionId,
+			username: session.username,
+			expires: session.expires
+		});
+		if (response[0].result[0]) {
+			return session;
+		} else {
+			throw new Error('Unable to create session for user: ' + username);
+		}
+	}
+
+	async deleteSesssionById(sessionId: string) {
+		const response = await this._db.delete(sessionId);
+	}
+
+	/**
+	 * GETTER AND SETTER
+	 */
 	get db() {
 		return this._db;
 	}
