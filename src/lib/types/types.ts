ff2eb5f04c61e1a0b0d45394c95c4dabaf153be7
Christoffer Dahl <coffedahl@hotmail.com>
2023-06-02 13:19:38 +0200

    Added type for sessiondata

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,5 +1,8 @@
 export interface WebsiteData {
-    name: string;
-    pageurl: string;
-    imgUrl: string;
+	name: string;
+	pageurl: string;
+	imgUrl: string;
+}
+export interface SessionData {
+	sessionId: string;
 }
