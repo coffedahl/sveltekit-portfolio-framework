feb4b2118dee6fdccc3922471f48f6629283401b
Christoffer Dahl <coffedahl@hotmail.com>
2023-06-02 21:47:42 +0200

    Updated imports

--- src/routes/web/+page.svelte
+++ src/routes/web/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import type { WebsiteData } from '$lib/types';
-	import Webdisplay from './components/webdisplay.svelte';
+	import type { WebsiteData } from '$lib/types/types';
+	import Webdisplay from '../../components/webdisplay.svelte';
 	const array: WebsiteData[] = [
 		{ name: 'Coffe Dahl', imgUrl: '', pageurl: '/coffedahl' },
-		{ name: 'JDTEK', imgUrl: '', pageurl: '/jdtek'},
+		{ name: 'JDTEK', imgUrl: '', pageurl: '/jdtek' },
 		{ name: 'Må ditt bästa', imgUrl: '/images/madittbasta.png', pageurl: '/madittbasta' },
 		{ name: 'Open-SP', imgUrl: '/images/open-sp.png', pageurl: '/open-sp' }
 	];
 </script>
 
 <div class="main">
-	<section class="flex">
+	<section class="flex total-center">
 		{#each array as i}
 			<div class="site">
 				<Webdisplay webdata={i} />
@@ -26,7 +26,7 @@
 		margin-left: 2.5%;
 		margin-top: 2.5%;
 	}
-	section{
+	section {
 		flex-wrap: wrap;
 	}
 </style>
