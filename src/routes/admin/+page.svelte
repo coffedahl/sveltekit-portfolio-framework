69d55762234d726ce9b29765be70fdd390842095
Christoffer Dahl <coffedahl@hotmail.com>
2023-06-03 10:46:59 +0200

    Fixed temporary styling

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,4 +1,6 @@
-<h1>Admin</h1>
-<form method="post">
-	<button>Logout</button>
-</form>
+<div class="main flex flex-column total-center">
+	<h1>Admin</h1>
+	<form method="post">
+		<button>Logout</button>
+	</form>
+</div>
