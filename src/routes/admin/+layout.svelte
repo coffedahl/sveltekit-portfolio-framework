4b9c4e67995d16945a4ab0e5afbeb643e5a6c108
Christoffer Dahl <coffedahl@hotmail.com>
2023-06-06 20:51:02 +0200

    Created a layout for admin page

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,16 @@
+<div class="main flex flex-column justify-even items-center">
+	<h1>Admin</h1>
+	<slot />
+	<form method="post">
+		<button class="btn logout-button">Logout</button>
+	</form>
+</div>
+
+<style lang="scss">
+	.menu-button {
+		padding: 1.5em;
+	}
+	.logout-button {
+		padding: 0.8em;
+	}
+</style>
