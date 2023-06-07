ee6dc4eb3baf5f32e8d35ac4b7b1b027acf449d5
Christoffer Dahl <coffedahl@hotmail.com>
2023-06-07 19:21:11 +0200

    Updated styling on website pages

--- src/routes/admin/website/[slug]/+page.svelte
+++ src/routes/admin/website/[slug]/+page.svelte
@@ -1,16 +1,28 @@
 <script lang="ts">
 	import type { PageServerData } from './$types';
 	export let data: PageServerData;
-	$: website = data.website;
 </script>
 
 <div class="flex flex-column">
-	<h1>{website.name}</h1>
+	<h1>{data.website.name}</h1>
 	<form method="post" class="flex flex-column items-center justify-even">
-		<input type="text" name="name" value={website.name} />
-		<input type="text" name="id" value={website.id.split(':')[1]} />
-		<input type="file" name="image" value={website.imgUrl} />
-		<input type="text" name="publicUrl" value={website.pubUrl} />
+		<div>
+			<b>Namn</b>
+			<input type="text" name="name" value={data.website.name} />
+		</div>
+		<div>
+			<b>Id</b>
+			<input type="text" name="id" value={data.website.id.split(':')[1]} />
+		</div>
+		<div class="flex flex-column">
+			<b>Image</b>
+			<img style="width: 10vw" src={data.website.imgUrl} alt="" />
+			<input type="file" name="image" />
+		</div>
+		<div>
+			<b>Publik URL</b>
+			<input type="text" name="publicUrl" value={data.website.publicUrl} />
+		</div>
 		<button>Save</button>
 	</form>
 </div>
