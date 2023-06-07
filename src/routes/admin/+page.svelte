efbe2136bbe8b70919b048da82b06f2b319872a2
coffedahl <coffedahl@hotmail.com>
2023-06-08 00:13:57 +0200

    Removed unused script and styled page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,10 +1,12 @@
-<script lang="ts">
-	import { Website } from '$lib/classes/websites';
-	import type { PageServerData } from './$types';
-	export let data: PageServerData;
-</script>
-
-<div class="menu">
+<div class="menu flex flex-column items-center">
 	<a href="/admin/website"><button class="btn menu-button">Manage websites</button></a>
 	<a href="/admin/photo"><button class="btn menu-button">Manage photos</button></a>
 </div>
+
+<style>
+	button{
+		width: 20vw;
+		height: 5vh;
+		margin-top: 1em;
+	}
+</style>
