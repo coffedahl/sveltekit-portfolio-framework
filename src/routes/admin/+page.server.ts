2d1702561b5ec28a1a330a154f2771a85ee88316
Christoffer Dahl <coffedahl@hotmail.com>
2023-06-07 18:10:57 +0200

    Added comments

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,24 +1,25 @@
 import { redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
-import type { WebData } from '$lib/classes/websites';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	const webData = await locals.db.getWebsiteList();
-	const webList: WebData[] = [];
-	webData.forEach((website) => {
-		webList.push(website.getDataAsObject());
-	});
-	return { webList };
+	// Return data
+	return { webList: locals.webData };
 };
 
 export const actions: Actions = {
+	/**
+	 * Function for handling logout
+	 */
 	default: async ({ cookies, locals }) => {
+		// Get session from cookie
 		const sessionData = JSON.parse(String(cookies.get('session')));
 		if (sessionData) {
+			// Delete the session from the database
 			if (!(await locals.db.deleteSessionById(sessionData.sessionId))) {
 				throw Error('Unable to delete session');
 			}
 		}
+		// Delete cookie and redirect
 		cookies.delete('session');
 		throw redirect(303, '/');
 	}
